c3aea3709f323b27354db7129dadb48fa26752e5
sohyeonAn <ansh4393@gmail.com>
2022-01-05 10:27:46 +0900

    🎨 textarea, 이미지 추가 버튼 리스너 추가

---
+++ js/upload.js
@@ -0,0 +1,65 @@
+document
+  .querySelector(".btn-img-upload")
+  .addEventListener("click", () => document.querySelector(".inp-imgs").click());
+
+function previewFiles() {
+  var preview = document.querySelector(".cont-preview");
+  var files = document.querySelector(".inp-imgs").files;
+
+  function readAndPreview(file) {
+    // `file.name` 형태의 확장자 규칙에 주의하세요
+    if (/\.(jpe?g|png|gif)$/i.test(file.name)) {
+      var reader = new FileReader();
+
+      reader.addEventListener(
+        "load",
+        function () {
+          const img = document.createElement("img");
+          img.classList.add("img-preview");
+          img.src = this.result;
+          preview.appendChild(img);
+        },
+        false
+      );
+
+      reader.readAsDataURL(file);
+    }
+  }
+
+  if (files) {
+    [].forEach.call(files, readAndPreview);
+  }
+}
+
+function fnChkByte(obj) {
+  const maxByte = 500;
+  const str = obj.value;
+  let currByte = 0,
+    endIdx = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (escape(str.charAt(i)) > 4) {
+      currByte += 2;
+    } else {
+      currByte += 1;
+    }
+
+    if (currByte >= maxByte) {
+      endIdx = i;
+      break;
+    }
+  }
+
+  if (endIdx === 0) {
+    console.log(str);
+    obj.innerText = str;
+  } else {
+    alert(`메세지는 최대 ${maxByte}바이트까지 입력 가능합니다.`);
+    obj.innerText = str.substring(0, endIdx);
+  }
+}
+
+function resize(obj) {
+  obj.style.height = "1px";
+  obj.style.height = 12 + obj.scrollHeight + "px";
+}
