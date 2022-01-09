d1329127b7f8d05b83d19e5a95a387c731892dbb
sohyeonAn <ansh4393@gmail.com>
2022-01-09 13:16:53 +0900

    🚧 [#2]  test commit

--- js/post.js
+++ js/post.js
@@ -1,13 +1,12 @@
-//refresh-token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2NDEzNzg2ODQsImV4cCI6MTY0MjU4ODI4NH0.oX1QmdQG36F7NqX7CgJGIwfOhzLCXhgSMMqxtyIbciE
 //token(ash__h): eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZDU3MjdjNmI4MjE2ZmM1NjY4NzZhOSIsImV4cCI6MTY0NjU2MjY4NCwiaWF0IjoxNjQxMzc4Njg0fQ.TBRQv7LmYSlN92I8ZYtf8ly1DomJ55MAIwc042YMv4g
 //token(ash2): eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZDU3ODMxNmI4MjE2ZmM1NjY4NzZlZCIsImV4cCI6MTY0NjU2Mzk2OSwiaWF0IjoxNjQxMzc5OTY5fQ.ugws0yLMbn0G4dKLwPSDTHPz-e3TmG7HeO_lXC8y-PM
 const TEST_TOKEN =
   "Bearer " +
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZDU3MjdjNmI4MjE2ZmM1NjY4NzZhOSIsImV4cCI6MTY0NjU2MjY4NCwiaWF0IjoxNjQxMzc4Njg0fQ.TBRQv7LmYSlN92I8ZYtf8ly1DomJ55MAIwc042YMv4g";
-// const TEST_POST_ID = "61d576a66b8216fc566876d2";
-// const TEST_POST_ID = "61d6df2b685c75821c469db4"; // 이미지 1장, 댓글 있음
-// const TEST_POST_ID = "61d7f550685c75821c46aca5"; // 이미지 2장, 댓글 없음
-const TEST_POST_ID = "61d7fc4a685c75821c46ad08"; // 글만 있음, 댓글 없음
+const TEST_POST_ID = "61d576a66b8216fc566876d2"; // ash__H, 이미지 1장, 댓글 있음
+// const TEST_POST_ID = "61d6df2b685c75821c469db4"; // ash2, 이미지 1장, 댓글 있음
+// const TEST_POST_ID = "61d7f550685c75821c46aca5"; // ash2, 이미지 2장, 댓글 없음
+// const TEST_POST_ID = "61d7fc4a685c75821c46ad08"; // ash2, 글만 있음, 댓글 있음
 const URL = "http://146.56.183.55:5050";
 
 const HEADER = new Headers({
@@ -282,3 +281,5 @@ likeBtn.addEventListener("click", likeBtnClickHandler);
 
 // init
 getPost(TEST_POST_ID);
+
+const test = "칸반보드 테스트 중~";
